cf21fd39556011e2b1509d965fb06ef71680a509
Toan Vo <toan@tinypulse.com>
2015-12-10 20:47:10 +0700

    implement login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create, :login]
+
   def index
+    @users = User.all
   end
 
   def new
@@ -7,13 +10,31 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.find_by_email(params[:email])
 
-    if @user.save
-      render :index
+    if @user
+      if @user.try(:authenticate, params[:password])
+        session[:user_id] = @user.id
+        redirect_to :index
+      else
+        redirect_to root_path
+      end
     else
-      render :new
+      @user = User.new(user_params)
+      if @user.save!
+        session[:user_id] = @user.id
+        redirect_to :index
+      else
+        render :new
+      end
     end
+  end
+
+  def login
+  end
+
+  def logout
+    session[:user_id] = nil
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :users
+  get 'users/login', to: 'users#login', as: 'login_users'
+  delete 'users/logout', to: 'users#logout', as: 'logout_users'
 
   get 'welcome/index'
 
